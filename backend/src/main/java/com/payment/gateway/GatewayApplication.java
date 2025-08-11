2713aba11419b0257ef79567023a01a1af3f7bcc
Baran Cil <barancil95@gmail.com>
2025-08-11 13:31:27 +0300

    jwt ile ilgili classlar eklendi

--- backend/src/main/java/com/payment/gateway/GatewayApplication.java
+++ backend/src/main/java/com/payment/gateway/GatewayApplication.java
@@ -1,15 +1,26 @@
 package com.payment.gateway;
 
+import com.payment.gateway.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class GatewayApplication {
+@RequiredArgsConstructor
+public class GatewayApplication implements CommandLineRunner {
+
+	private final UserService userService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// Create default users on startup
+		userService.createDefaultUsers();
+	}
 }
